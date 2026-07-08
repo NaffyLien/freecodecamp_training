3c74f88c893d7e8af36d9d7fb0786bc7a26f4698
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-07-08 21:38:29 +0300

    Project: Build a Travel Weather Planner In this part, we practice the usage of condition simple, with else and else if. We checked the nested condition, trusy and falsy conditions, condition with binary operations
    Subject from FreeCodeCamp:
    Build a Travel Weather Planner
    
    For this lab, you will use conditional statements to determine whether commuting is possible based on the weather, the distance to travel, and the availability of a vehicle.
    
    Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
    
    User Stories:
    
        You should create the following variables:
            distance_mi (a number representing the distance to travel in miles)
            is_raining (a boolean representing if the user is currently experiencing rainy weather)
            has_bike (a boolean representing if the user has a bicycle)
            has_car (a boolean representing if the user has a car)
            has_ride_share_app (a boolean representing if the user has an app that allows them to request a ride)
        You should use conditional statements to determine whether commuting is possible based on the values of these variables.
        You should use if, elif, and else statements to evaluate the distance categories in ascending order.
        If distance_mi is a falsy value:
            You should print False.
        If the distance is less than or equal to 1 mile:
            You should print True only if it is not raining.
            Otherwise, you should print False.
        If the distance is greater than 1 mile and less than or equal to 6 miles:
            You should print True only if the person has a bike and it is not raining.
            Otherwise, you should print False.
        If the distance is greater than 6 miles:
            You should print True if the person has a car or has a ride-share app.
            Otherwise, you should print False.
    
    Tests:
    
        Passed: 1. You should have a variable named distance_mi.
        Passed: 2. You should assign a number to your distance_mi variable.
        Passed: 3. You should have a variable named is_raining.
        Passed: 4. You should assign a boolean to your is_raining variable.
        Passed: 5. You should have a variable named has_bike.
        Passed: 6. You should assign a boolean to your has_bike variable.
        Passed: 7. You should have a variable named has_car.
        Passed: 8. You should assign a boolean to your has_car variable.
        Passed: 9. You should have a variable named has_ride_share_app.
        Passed: 10. You should assign a boolean to your has_ride_share_app variable.
        Passed: 11. You should use at least one if statement.
        Passed: 12. You should use at least one elif branch in your program.
        Passed: 13. You should use at least one boolean operator (and, or, or not) in your code.
        Passed: 14. You should use the print() function to display the result.
        Passed: 15. When distance_mi is a falsy value, the program should print False.
        Passed: 16. When the distance is 1 mile or less and it is not raining, the program should print True.
        Passed: 17. When the distance is 1 mile or less and it is raining, the program should print False.
        Passed: 18. When the distance is between 1 mile (excluded) and 6 miles (included), and it is raining with no bike, the program should print False.
        Passed: 19. When the distance is between 1 mile (excluded) and 6 miles (included), it is not raining but no bike is available, the program should print False.
        Passed: 20. When the distance is between 1 mile (excluded) and 6 miles (included), a bike is available, and it is not raining, the program should print True.
        Passed: 21. When the distance is greater than 6 miles and a ride share app is available, the program should print True.
        Passed: 22. When the distance is greater than 6 miles and a car is available, the program should print True.
        Passed: 23. When the distance is greater than 6 miles and no car nor a ride share app is available, the program should print False.

--- script.py
+++ script.py
@@ -1,48 +1,23 @@
-base_price = 15
-age = 21
-seat_type = 'Gold'
-show_time = 'Evening'
+distance_mi = 7
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = False
 
-if age > 17:
-    print('User is eligible to book a ticket')
+if not is_raining and (has_bike or has_car or has_ride_share_app) :
+    commuting = True
 
-if age >= 21:
-    print('User is eligible for Evening shows')
-else:
-    print('User is not eligible for Evening shows')
-
-is_member = False
-is_weekend = False
-
-discount = 0
-if is_member and age >= 21:
-    discount = 3
-    print('User qualifies for membership discount')
-else:
-    print('User does not qualify for membership discount')
-print('Discount:', discount)
-
-extra_charges = 0
-if is_weekend or show_time == 'Evening':
-    extra_charges = 2
-    print('Extra charges will be applied')
-else:
-    print('No extra charges will be applied')
-print('Extra charges:', extra_charges)
-
-if age >= 21 or age >= 18 and (show_time != 'Evening' or is_member):
-    print('Ticket booking condition satisfied')
-
-    service_charges = 0
-    if seat_type == 'Premium':
-        service_charges = 5
-    elif seat_type == 'Gold':
-        service_charges = 3
-    else:
-        service_charges = 1
-    print('Service charges:', service_charges)
-
-    final_price = extra_charges + service_charges + base_price - discount
-    print("Final price of ticket:",final_price)
-else:
-    print('Ticket booking failed due to restrictions')
+if not distance_mi:
+    print("False")
+elif distance_mi <=1 :
+    if not is_raining:
+        print("True")
+    else: print("False")
+elif distance_mi > 1 and distance_mi <= 6:
+    if has_bike and not is_raining:
+        print("True")
+    else: print("False")
+elif distance_mi > 6:
+    if has_car or has_ride_share_app:
+        print("True")
+    else: print("False")
