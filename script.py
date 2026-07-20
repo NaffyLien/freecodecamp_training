ebedc99c23ff1ad1d21ed50fb1a15afcd49d4bde
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-07-20 20:14:21 +0300

    Build a Number Pattern Generator
    In this lab you will practice the basics of Python by building a small app that creates a number pattern.
    
    Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
    
    User Stories:
    
        You should define a function named number_pattern that takes a single parameter n (representing a positive integer).
        number_pattern should use a for loop.
        number_pattern(n) should return a string with all the integers starting from 1 up to n (included) separated by a space. For example, number_pattern(4) should return the string 1 2 3 4.
        If the argument passed to the function is not an integer value, the function should return Argument must be an integer value.
        If the argument passed to the function is less than 1, the function should return Argument must be an integer greater than 0.

--- script.py
+++ script.py
@@ -1,23 +1,14 @@
-def pin_extractor(poems):
-    secret_codes = []
-    for poem in poems:
-        secret_code = ''
-        lines = poem.split('\n')
-        for line_index, line in enumerate(lines):
-            words = line.split()
-            if len(words) > line_index:
-                secret_code += str(len(words[line_index]))
-            else:
-                secret_code += '0'
-        secret_codes.append(secret_code)
-    return secret_codes
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
 
-poem = """Stars and the moon
-shine in the sky
-white and
-until the end of the night"""
+    if n <= 1:
+        return "Argument must be an integer greater than 0."
 
-poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
-poem3 = 'There\nonce\nwas\na\ndragon'
+    pattern = []
+    for i in range(n):
+        pattern.append(str(i+1))
+    pattern = " ".join(pattern)
+    return pattern
 
-print(pin_extractor([poem, poem2, poem3]))
+print(number_pattern(5))
